76d9d6d00866c45844464cb1f4a4a1594220c75a
maosx <maosx@sucsoft.com>
2023-02-10 17:44:01 +0800

    feat: 原型模式

---
+++ 创建型模式/05-原型模式/PrototypePatternDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 原型模式：https://www.runoob.com/design-pattern/prototype-pattern.html
+ */
+public class PrototypePatternDemo {
+
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+
+}
